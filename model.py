2010edcd3f2a318fe020fa17ccbe013f787ce10a
ColesonWelles <cahh38@protonmail.com>
2024-07-03 23:58:44 -0700

    apply_rotary_embeddings progress

--- model.py
+++ model.py
@@ -42,6 +42,17 @@ def precompute_theta_pos_frequencies(head_dim: int, seq_len: int, device: str, t
     freqs_complex = torch.polar(torch.ones_like(freqs), freqs)
     return freqs_complex
 
+def apply_rotary_embeddings(x: torch.Tensor, freqs_complex: torch.Tensor, device: str):
+    # (batch, seq_len, h, head_dim) -> (batch, seq_len, h, head_dim / 2)
+    x_complex = torch.view_as_complex(x.float().reshape(*x.shape[:-1], -1, 2))
+    # (seq_len, head_dim / 2) -> (1, seq_len, 1, head_dim / 2)
+    freqs_complex = freqs_complex.unsqueeze(0).unsqueeze(2)
+    # (batch, seq_len, h, head_dim / 2) * (1, seq_len, 1, head_dim / 2) = (batch, seq_len, h, head_dim / 2)
+    x_rotated = x_complex * freqs_complex
+    # (batch, seq_len, h, head_dim / 2) -> (batch, seq_len, h, head_dim / 2, 2)
+    x_out = torch.view_as_real(x_rotated)
+    x_out = x_out.reshape(*x.shape)
+
 
 class RMSNorm(nn.Module):
      
