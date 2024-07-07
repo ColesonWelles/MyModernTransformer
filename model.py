4c529f1d1e21673f258e9ceef0baf00e9fa50400
ColesonWelles <cahh38@protonmail.com>
2024-07-06 22:23:19 -0700

    FeedForward w/ SwiGLU  module progres

--- model.py
+++ model.py
@@ -72,6 +72,19 @@ class RMSNorm(nn.Module):
     def forward(self, x: torch.Tensor):
         # (dim) * (batch, seq_len, dim) = (batch, seq_len, dim)
         return self.weight * self._norm(x.float()).type_as(x)
+    
+
+class FeedForward(nn.Module):
+
+    def __init__(self, args: ModelArgs):
+        super().__init__()
+
+        hidden_dim = 4 * args.dim
+        hidden_dim = int(2 * hidden_dim / 3)
+        if args.ffn_dim_multiplier is not None:
+            hidden_dim = int(args.ffn_dim_multiplier * hidden_dim)
+        # Round the hidden_dim to the nearest multiple of the multiple of parameter
+        
 
 
 class EncoderBlock(nn.Module):
